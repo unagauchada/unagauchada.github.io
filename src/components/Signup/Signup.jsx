29d858a9d24758f30f82e7ad4cb65ff1f705f0ca
Matias Pierobon <matias.pierobonm@gmail.com>
2017-05-25 19:05:00 -0300

    add redirect for success signup

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Redirect } from "react-router-dom"
 import Paper from "react-md/lib/Papers"
 import Button from "react-md/lib/Buttons/Button"
 import TextField from "react-md/lib/TextFields"
@@ -10,7 +11,7 @@ import "./Signup.scss"
 class Singup extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { email: "", password: "", error: true }
+    this.state = { email: "", password: "", error: true, done: false }
   }
 
   handleChange = property => value => {
@@ -23,11 +24,13 @@ class Singup extends React.Component {
     firebase
       .auth(app)
       .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(_ =>  this.setState({ done: true }))
       .catch(error => this.setState({ error }))
   }
 
   render = () => (
     <signup>
+      { this.state.done && <Redirect to="/" /> }
       <Paper zDepth={2}>
         <header>Registrarse al sistema</header>
         <section>
@@ -54,7 +57,7 @@ class Singup extends React.Component {
           </div>
         </section>
         <footer>
-          <Button raised label="Aceptar" primary onClick={this.createUser} />
+          <Button raised label="Aceptar" disable={ this.state.done === true } primary onClick={this.createUser} />
         </footer>
       </Paper>
     </signup>
