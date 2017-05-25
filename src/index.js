17117069e5a3ee262b97b2968fa04f592ba9f452
Matias Pierobon <matias.pierobonm@gmail.com>
2017-05-25 16:34:06 -0300

    add webfontloader to index
    Add Roboto fonts, Material Icons and Product Sans

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from "webfontloader"
+import App from "./components/App"
+
+WebFont.load({
+  google: {
+    families: ["Roboto:200,300,600", "Material Icons", "Product Sans:200,300,600"]
+  }
+})
 
 ReactDOM.render(
   <App />,
