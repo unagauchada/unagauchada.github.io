0c3775da26d0bbadaec6276546e17ff3f54b01d6
Matias Pierobon <matias.pierobonm@gmail.com>
2017-05-25 21:17:11 -0300

    add custom data to user signup

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,42 +1,96 @@
+// @flow
+
 import React from "react"
 import { Redirect } from "react-router-dom"
+import _ from "lodash"
 import Paper from "react-md/lib/Papers"
 import Button from "react-md/lib/Buttons/Button"
 import TextField from "react-md/lib/TextFields"
+import SelectField from "react-md/lib/SelectFields"
+import DatePicker from "react-md/lib/Pickers/DatePickerContainer"
 import firebase from "firebase"
-import { app } from "../../libs/db"
-
+import rootRef, { app } from "../../libs/db"
 import "./Signup.scss"
 
+type State = {
+  email: string,
+  password: string,
+  name: string,
+  lastname: string,
+  phone: string,
+  city: any,
+  birthdate: Date,
+  error: any,
+  done: boolean,
+  stateItems: Array<{ name: string, id: any }>
+}
+type Props = { match: any }
+
 class Singup extends React.Component {
-  constructor(props) {
+  state: State
+
+  constructor(props: Props) {
     super(props)
-    this.state = { email: "", password: "", error: true, done: false }
+    this.state = {
+      email: "",
+      password: "",
+      name: "",
+      lastname: "",
+      city: null,
+      phone: "",
+      birthdate: new Date(),
+      error: false,
+      done: false,
+      stateItems: [{ name: " loading ", id: -1 }]
+    }
   }
 
-  handleChange = property => value => {
+  handleChange = (property: string) => (value: string) => {
     this.setState({
       [property]: value
     })
   }
 
   createUser = () => {
+    let { name, lastname, city, birthdate, phone } = this.state
     firebase
       .auth(app)
       .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(_ =>  this.setState({ done: true }))
+      .then(user => {
+        user.updateProfile({
+          displayName: `${this.state.name} ${this.state.lastname[0].toUpperCase()}.`
+        })
+        return user
+      })
+      .then(user => rootRef.child('users').child(user.uid).set({ name, lastname, city, birthdate, phone  }))
+      .then(() => this.setState({ done: true }))
       .catch(error => this.setState({ error }))
   }
 
+  componentDidMount() {
+    this.getStates()
+  }
+
+  getStates = () => {
+    rootRef.child("states").on(
+      "value",
+      snap => {
+        this.setState({
+          stateItems: _.map(snap.val(), (state, id) => ({ ...state, id }))
+        })
+      },
+      err => console.error(err)
+    )
+  }
   render = () => (
     <signup>
-      { this.state.done && <Redirect to="/" /> }
+      {this.state.done && <Redirect to="/" />}
       <Paper zDepth={2}>
         <header>Registrarse al sistema</header>
-        <section>
+        <section className="md-grid">
           {this.state.error &&
             <div className="error">{this.state.error.message}</div>}
-          <div>
+          <div className="md-cell md-cell--12">
             <TextField
               id="email"
               label="Email"
@@ -45,7 +99,7 @@ class Singup extends React.Component {
               onChange={this.handleChange("email")}
             />
           </div>
-          <div>
+          <div className="md-cell md-cell--12">
             <TextField
               id="password"
               label="Contraseña"
@@ -55,9 +109,57 @@ class Singup extends React.Component {
               onChange={this.handleChange("password")}
             />
           </div>
+          <div className="md-cell md-cell--6">
+            <TextField
+              id="name"
+              label="Nombre"
+              fullWidth={true}
+              value={this.state.name}
+              onChange={this.handleChange("name")}
+            />
+          </div>
+          <div className="md-cell md-cell--6">
+            <TextField
+              id="lastname"
+              label="Apellido"
+              fullWidth={true}
+              value={this.state.lastname}
+              onChange={this.handleChange("lastname")}
+            />
+          </div>
+          <div className="md-cell md-cell--12">
+            <SelectField
+              id="city"
+              menuItems={this.state.stateItems}
+              label="Ciudad"
+              fullWidth={true}
+              itemLabel="name"
+              itemValue="id"
+              value={this.state.city}
+              onChange={this.handleChange("city")}
+            />
+          </div>
+          <div className="md-cell md-cell--6">
+            <TextField
+              id="cell"
+              label="Teléfono"
+              fullWidth={true}
+              value={this.state.phone}
+              onChange={this.handleChange("phone")}
+            />
+          </div>
+          <div className="md-cell md-cell--6">
+            <DatePicker
+              id="birthdate"
+              label="Cumpleaños"
+              fullWidth={true}
+              value={this.state.birthdate}
+              onChange={this.handleChange("birthdate")}
+            />
+          </div>
         </section>
         <footer>
-          <Button raised label="Aceptar" disable={ this.state.done === true } primary onClick={this.createUser} />
+          <Button raised label="Aceptar" primary onClick={this.createUser} />
         </footer>
       </Paper>
     </signup>
