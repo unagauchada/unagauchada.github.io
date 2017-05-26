1fbd2cef28d34f3f083cbfb7ff08ef09d3ee972e
Matias Pierobon <matias.pierobonm@gmail.com>
2017-05-26 04:09:46 -0300

    create HomePage displaying the publication list

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import MainPage from '../MainPage'
+import PublicationList from '../PublicationList'
+
+const Home = () => (
+    <MainPage>
+        <PublicationList />
+    </MainPage>
+)
+
+export default Home
