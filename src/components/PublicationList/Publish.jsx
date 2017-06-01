be05f7c15e400ad97657dd62409db1e5908787a5
Matias Pierobon <matias.pierobonm@gmail.com>
2017-06-01 04:12:28 -0300

    format publish and add user avatar

--- src/components/PublicationList/Publish.jsx
+++ src/components/PublicationList/Publish.jsx
@@ -1,31 +1,37 @@
 import React from "react"
 import { connect } from "react-redux"
-import Avatar from "react-md/lib/Avatars"
 import FontIcon from "react-md/lib/FontIcons"
 import Button from "react-md/lib/Buttons/Button"
 import TextField from "react-md/lib/TextFields"
-import SelectField from 'react-md/lib/SelectFields';
+import SelectField from "react-md/lib/SelectFields"
 
 import { userSelector } from "../../redux/getters"
 import rootRef, { storageRef } from "../../libs/db"
-import ImageUpload from './ImageUpload';
+import UserAvatar from "../UserAvatar"
+import ImageUpload from "./ImageUpload"
 import firebase from "firebase"
 
-const stateItems = [{
-  name: "La Plata",
-  value: 1
-},{
-  name: "otro",
-  value: 2
-}]
+const stateItems = [
+  {
+    name: "La Plata",
+    value: 1
+  },
+  {
+    name: "otro",
+    value: 2
+  }
+]
 
-const categoryItems = [{
-  name: "Entretenimiento",
-  value: 1
-},{
-  name: "Otros",
-  value: 2
-}]
+const categoryItems = [
+  {
+    name: "Entretenimiento",
+    value: 1
+  },
+  {
+    name: "Otros",
+    value: 2
+  }
+]
 
 const duration = 15
 const thisDate = new Date()
@@ -35,7 +41,6 @@ class Publish extends React.Component {
   constructor(props) {
     super(props)
 
-
     this.state = {
       title: "",
       category: 1,
@@ -43,63 +48,63 @@ class Publish extends React.Component {
       submissions: 0,
       text: "",
       end: thisDate.setDate(thisDate.getDate() + duration),
-      image: {url: "", file: null}
+      image: { url: "", file: null }
     }
   }
 
   getUserId = () => {
-    var uid;
+    var uid
 
     if (this.props.user != null) {
-        uid = this.props.user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                        // this value to authenticate with your backend server, if
-                        // you have one. Use User.getToken() instead.
+      uid = this.props.user.uid // The user's ID, unique to the Firebase project. Do NOT use
+      // this value to authenticate with your backend server, if
+      // you have one. Use User.getToken() instead.
     }
 
     return uid
   }
 
   submit = () => {
-
     let { title, category, state, submissions, text, end } = this.state
     let imageURL = ""
     let user = this.props.user.uid
     let key = rootRef
-                .child("publications")
-                .push({ title, category, state, submissions, text, end, imageURL, user },() => this.clean()).key
+      .child("publications")
+      .push(
+        { title, category, state, submissions, text, end, imageURL, user },
+        () => this.clean()
+      ).key
 
     console.log(this.state.image.file)
     console.log(key)
     //image uploading to Firebase
     let uploadTask = storageRef
-                        .child('publications/'.concat(key))
-                        .put(this.state.image.file);
+      .child("publications/".concat(key))
+      .put(this.state.image.file)
 
-    uploadTask.on('state_changed', function(snapshot){
-      // Observe state change events such as progress, pause, and resume
-      // See below for more detail
-    }, function(error) {
-      // Handle unsuccessful uploads
-    }, () => {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      this.state.image.url = uploadTask.snapshot.downloadURL;
-      
-      console.log(this.state.image.url)
-      rootRef
-        .child("publications/".concat(key).concat("/imageURL"))
-        .set(this.state.image.url)
+    uploadTask.on(
+      "state_changed",
+      function(snapshot) {},
+      function(error) {
+        console.error(error)
+      },
+      () => {
+        this.state.image.url = uploadTask.snapshot.downloadURL
 
-      rootRef
-        .child("users/" + this.getUserId().toString() + "/credits")
-        .once(
-          "value",
-          snap => 
-              rootRef
-                  .child("users/" + this.getUserId().toString() + "/credits")
-                  .set(snap.val() - 1))    
-      });
-}
+        rootRef
+          .child("publications/".concat(key).concat("/imageURL"))
+          .set(this.state.image.url)
+
+        rootRef
+          .child("users/" + this.getUserId().toString() + "/credits")
+          .once("value", snap =>
+            rootRef
+              .child("users/" + this.getUserId().toString() + "/credits")
+              .set(snap.val() - 1)
+          )
+      }
+    )
+  }
 
   cancel = () => this.clean()
 
@@ -108,17 +113,17 @@ class Publish extends React.Component {
     this.props.handleClose()
   }
 
-  _handleStateChange = (value, index, event) => { // eslint-disable-line no-unused-vars
-    this.setState({ state: value });
-  };
+  _handleStateChange = (value, index, event) => {
+    this.setState({ state: value })
+  }
 
-  _handleCategoryChange = (value, index, event) => { // eslint-disable-line no-unused-vars
-    this.setState({ category: value });
-  };
+  _handleCategoryChange = (value, index, event) => {
+    this.setState({ category: value })
+  }
 
   handleChange = property => value => this.setState({ [property]: value })
 
-  setImage = (image) => {
+  setImage = image => {
     let state = this.state
     state.image.file = image
     this.setState(state)
@@ -126,66 +131,67 @@ class Publish extends React.Component {
 
   render = () => {
     let placepicker = null
-    return(
-    <section className="dialog md-grid">
-      <section className="header md-cell md-cell--2 md-cell--middle md-text-center ">
-        <Avatar icon={<FontIcon>person</FontIcon>} />
-      </section>
-      <TextField
-        id="floatingTitle"
-        label="Title"
-        customSize="title"
-        size={10}
-        className="md-cell md-cell--10 md-cell--middle"
-        value={this.state.title}
-        onChange={this.handleChange('title')}
-      />
-      <TextField
-        block
-        paddedBlock
-        id="body"
-        placeholder="Descripción"
-        rows={4}
-        value={this.state.text}
-        onChange={this.handleChange('text')}
-      />
+    return (
+      <section className="dialog md-grid">
+        <section className="header md-cell md-cell--2 md-cell--middle md-text-center ">
+          <UserAvatar url={this.props.user.photoURL} />
+        </section>
+        <TextField
+          id="floatingTitle"
+          label="Title"
+          customSize="title"
+          size={10}
+          className="md-cell md-cell--10 md-cell--middle"
+          value={this.state.title}
+          onChange={this.handleChange("title")}
+        />
+        <TextField
+          block
+          paddedBlock
+          id="body"
+          placeholder="Descripción"
+          rows={4}
+          value={this.state.text}
+          onChange={this.handleChange("text")}
+        />
 
-      <section className="md-cell md-cell--12">
-        <SelectField
-          primary
-          id="state"
-          label="Lugar"
-          itemLabel="name"
-          itemValue="value"
-          value={this.state.state}
-          menuItems={stateItems}
-          onChange={this._handleStateChange}
-          required
-          errorText="Debes seleccionar un lugar para el favor"
-          className="md-cell"
-          leftIcon={<FontIcon>place</FontIcon>}
-        />
-        <SelectField
-          primary
-          id="category"
-          label="Categoria"
-          itemLabel="name"
-          itemValue="value"
-          value={this.state.category}
-          menuItems={categoryItems}
-          onChange={this._handleCategoryChange}
-          required
-          errorText="Debes seleccionar una categoria para el favor"
-          className="md-cell"
-        />
-        <ImageUpload setImage={this.setImage}/>
+        <section className="md-cell md-cell--12">
+          <SelectField
+            primary
+            id="state"
+            label="Lugar"
+            itemLabel="name"
+            itemValue="value"
+            value={this.state.state}
+            menuItems={stateItems}
+            onChange={this._handleStateChange}
+            required
+            errorText="Debes seleccionar un lugar para el favor"
+            className="md-cell"
+            leftIcon={<FontIcon>place</FontIcon>}
+          />
+          <SelectField
+            primary
+            id="category"
+            label="Categoria"
+            itemLabel="name"
+            itemValue="value"
+            value={this.state.category}
+            menuItems={categoryItems}
+            onChange={this._handleCategoryChange}
+            required
+            errorText="Debes seleccionar una categoria para el favor"
+            className="md-cell"
+          />
+          <ImageUpload setImage={this.setImage} />
+        </section>
+        <section className="footer md-cell md-cell--12 md-text-right">
+          <Button flat label="Cancelar" onClick={this.cancel} />
+          <Button raised primary label="Publicar!" onClick={this.submit} />
+        </section>
       </section>
-      <section className="footer md-cell md-cell--12 md-text-right">
-        <Button flat label="Cancelar" onClick={this.cancel} />
-        <Button raised primary label="Publicar!" onClick={this.submit} />
-      </section>
-    </section>
-  )}
+    )
+  }
 }
 
 export default Publish
