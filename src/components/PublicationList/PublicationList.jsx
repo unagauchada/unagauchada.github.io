672883289f7c520d37507bb380184618849cdc10
Matias Pierobon <matias.pierobonm@gmail.com>
2017-06-08 00:59:52 -0300

    format publication and solve button

--- src/components/PublicationList/PublicationList.jsx
+++ src/components/PublicationList/PublicationList.jsx
@@ -3,11 +3,11 @@ import _ from "lodash"
 import { connect } from "react-redux"
 import Button from "react-md/lib/Buttons/Button"
 import Dialog from "react-md/lib/Dialogs"
-import Snackbar from 'react-md/lib/Snackbars';
+import Snackbar from "react-md/lib/Snackbars"
 
 import rootRef from "../../libs/db"
 import Publication from "./Publication"
-import Publish from './Publish'
+import Publish from "./Publish"
 import { userSelector } from "../../redux/getters"
 import "./PublicationList.scss"
 
@@ -26,27 +26,27 @@ class PublicationList extends React.Component {
     }
   }
 
-  _addToast=(text, action)=> {
-    const toasts = this.state.toasts.slice();
-    toasts.push({ text, action });
+  _addToast = (text, action) => {
+    const toasts = this.state.toasts.slice()
+    toasts.push({ text, action })
 
-    this.setState({ toasts });
+    this.setState({ toasts })
   }
 
-  _removeToast =() =>{
-    const [, ...toasts] = this.state.toasts;
-    this.setState({ toasts });
+  _removeToast = () => {
+    const [, ...toasts] = this.state.toasts
+    this.setState({ toasts })
   }
 
   toastFailure = () => {
-    this._addToast('No posee suficiente credito');
+    this._addToast("No posee suficiente credito")
   }
 
   componentDidMount = () => {
     this.getPublications()
   }
 
-  componentWillReceiveProps = (nextProps) => {
+  componentWillReceiveProps = nextProps => {
     this.getCredits(nextProps)
   }
 
@@ -70,45 +70,47 @@ class PublicationList extends React.Component {
       })
     })
 
-  getUserId = (props) => {
-    var uid;
+  getUserId = props => {
+    var uid
 
     if (props.user != null) {
-        uid = props.user.uid.toString();  // The user's ID, unique to the Firebase project. Do NOT use
-                        // this value to authenticate with your backend server, if
-                        // you have one. Use User.getToken() instead.
+      uid = props.user.uid.toString() // The user's ID, unique to the Firebase project. Do NOT use
+      // this value to authenticate with your backend server, if
+      // you have one. Use User.getToken() instead.
     }
 
     console.log(uid)
     return uid
   }
 
-  getCredits = (props) => {
+  getCredits = props => {
     rootRef
       .child("users/" + this.getUserId(props) + "/credits")
-      .on('value', snap => {
+      .on("value", snap => {
         console.log(snap.val())
-        this.setState({credits: snap.val()})      
-    })
+        this.setState({ credits: snap.val() })
+      })
   }
 
   getButton = () => {
-    let shadowed = (this.state.credits < publicationCost)
-    if (this.props.user && shadowed){ 
-      return(
-      <add>
-        <Button
-          className="add-publication"
-          floating
-          secundary
-          onClick={this.toastFailure}
+    let shadowed = this.state.credits && this.state.credits >= publicationCost
+    if (this.props.user && !shadowed) {
+      return (
+        <add>
+          <Button
+            className="add-publication"
+            floating
+            secundary
+            onClick={this.toastFailure}
           >
-          money_off
-        </Button>
-      </add>)
-    }else if(this.props.user){
-      return(
-        <add><Button
+            money_off
+          </Button>
+        </add>
+      )
+    } else if (this.props.user) {
+      return (
+        <add>
+          <Button
             className="add-publication"
             floating
             primary
@@ -122,26 +124,30 @@ class PublicationList extends React.Component {
             onHide={this.closeDialog}
             className="googleDialog"
           >
-          <Publish handleClose={this.closeDialog} />
-        </Dialog></add>)
-
+            <Publish handleClose={this.closeDialog} />
+          </Dialog>
+        </add>
+      )
     }
   }
 
   render = () => {
-    console.log(this.props.user)
     let publishButton = this.getButton()
-    return(
+    return (
       <publications>
-        {this.state.publications.sort((function(a, b){return b.submissions - a.submissions})).map(
-          publication =>
-            publication &&
-            <Publication key={publication.id} publication={publication} />
-        )}
-        { publishButton }
+        {this.state.publications
+          .sort(function(a, b) {
+            return b.submissions - a.submissions
+          })
+          .map(
+            publication =>
+              publication &&
+              <Publication key={publication.id} publication={publication} />
+          )}
+        {publishButton}
         <Snackbar {...this.state} onDismiss={this._removeToast} />
       </publications>
-  )
+    )
   }
 }
 
