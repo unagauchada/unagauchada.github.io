39c5fd493cfd3762219012e11ee18e2c1e29e85f
Fedearg <fedearg_612@hotmail.com>
2017-06-21 20:11:39 -0300

    tentative 1st version of search, pending review
    TO-DO:
    *Fix dialog size
    *Fix onChange
    *make compliant with use cases

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,16 @@
 // @flow
 import React from "react"
 import firebase from "firebase"
+import { Redirect } from 'react-router'
 import { HashRouter as Router, Route, Link, Switch } from "react-router-dom"
 import { connect } from "react-redux"
 import Toolbar from "react-md/lib/Toolbars"
 import Button from "react-md/lib/Buttons/Button"
-import TextField from "react-md/lib/TextFields"
 import FontIcon from "react-md/lib/FontIcons"
 import { app as fbApp } from "../../libs/db"
 import { userSelector } from "../../redux/getters"
 import { updateUser } from "../../redux/actions"
+import Divider from 'react-md/lib/Dividers'
 import Signup from "../Signup"
 import Login from "../Login"
 import Home from "../Home"
@@ -17,6 +18,8 @@ import BuyCredits from "../BuyCredits"
 import FavorView from "../Publication"
 import actionCreator from "../ToolbarActions"
 import MainPage from "../MainPage"
+
+import Autocomplete from 'react-md/lib/Autocompletes';
 import "./App.scss"
 
 const nav = <Button key="nav" icon>menu</Button>
@@ -38,11 +41,47 @@ const Error404 = () => (
 
 @connect(mapStateToProps, mapDispatchToProps)
 class App extends React.Component {
+  constructor() {
+        super();
+        this.state = {
+            searchText: "",
+            shouldRedir: false
+        };
+    }
+
   componentWillMount() {
     firebase.auth(fbApp).onAuthStateChanged(user => this.props.updateUser(user))
   }
 
+   handleClear() {
+    
+  }
+
+  handleChange= value =>{
+    if (value) {
+      this.setState({
+        searchText: value
+      });   
+  }
+  }
+
+  handleSelection = value => {
+    
+  }
+
+  redir = () => {
+    this.setState({shouldRedir: false})
+    return <Redirect to="/" />
+  }
+
+  handleSearch = () => {
+      this.setState({
+        shouldRedir: true
+      })      
+  }
+
   render = () => (
+    <form  onSubmit={(e) => {e.preventDefault(); this.handleSearch(); }}>
     <Router>
       <app>
         <Toolbar
@@ -52,31 +91,39 @@ class App extends React.Component {
             <h2>
               <Link to="/">Una<span>Gauchada</span></Link>
             </h2>
+
           }
           actions={actionCreator(this.props.user)}
           nav={nav}
         >
+
+          {this.state.shouldRedir? this.redir() : ""}
           <section>Explorar</section>
-          <TextField
-            id="iconLeftPhone"
-            block
-            placeholder="Buscar"
-            leftIcon={<FontIcon>search</FontIcon>}
-            size={10}
-            className="md-title--toolbar md-cell--middle toolbar-text"
-            inputClassName="md-text-field--toolbar"
-          />
+          <div className="md-cell--middle">
+          <Autocomplete
+              id="iconLeftPhone"
+              block
+              data={[]}
+              placeholder="Buscar"
+              leftIcon={<Button icon onClick={this.handleSearch}>search</Button>}
+              onChange={this.handleChange}
+              size={10}
+              className="md-title--toolbar md-cell--middle toolbar-text"
+              inputClassName="md-text-field--toolbar"
+            />
+          </div>
         </Toolbar>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={() => <Home searchText={this.state.searchText} />} />
           <Route path="/signup" component={Signup} />
           <Route path="/signin" component={Login} />
           <Route path="/buy" component={this.props.user ? BuyCredits : Login} />
           <Route path="/publication/:favorID"  component={this.props.user ? FavorView : Login } />
           <Route path="*" component={Error404} />
-        </Switch>
+        </Switch> 
       </app>
     </Router>
+    </form>
   )
 }
 
--- src/components/PublicationList/PublicationList.jsx
+++ src/components/PublicationList/PublicationList.jsx
@@ -2,14 +2,19 @@ import React from "react"
 import _ from "lodash"
 import { connect } from "react-redux"
 import Button from "react-md/lib/Buttons/Button"
+import Chip from 'react-md/lib/Chips';
 import Dialog from "react-md/lib/Dialogs"
-import Snackbar from "react-md/lib/Snackbars"
-
+import Snackbar from "react-md/lib/Snackbars";
+import Divider from 'react-md/lib/Dividers';
 import rootRef from "../../libs/db"
 import Publication from "./Publication"
+import SelectField from 'react-md/lib/SelectFields';
+import FontIcon from "react-md/lib/FontIcons"
 import Publish from "./Publish"
 import { userSelector } from "../../redux/getters"
 import "./PublicationList.scss"
+
+
 
 const publicationCost = 1
 
@@ -18,8 +23,13 @@ class PublicationList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      searchText: "",
+      searchLoc: "default",
+      searchCat: "default",
       publications: [],
       visible: false,
+      catVisible:false,
+      locVisible:false,
       credits: 0,
       toasts: [],
       autohide: true
@@ -44,10 +54,121 @@ class PublicationList extends React.Component {
 
   componentDidMount = () => {
     this.getPublications()
+    this.getStates()
+    this.getCategories()
   }
 
+  getStates = () => {
+    rootRef.child("states").on("value", snap =>
+      this.setState({
+        states: _.map(snap.val(), (state, value) => ({ ...state, value })).sort(
+          (a, b) => (a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1)
+        )
+      })
+    )
+  }
+
+  getCategories = () => {
+    rootRef.child("categories").on("value", snap =>
+      this.setState({
+        categories: _.map(snap.val(), (category, value) => ({ ...category, value})).sort(
+          (a, b) => (a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1)
+        )
+      })
+    )
+  }
+
+  componentWillMount = () => {
+    this.setState({searchText: this.props.searchText})
+  }
+
+  isSearching = () => {
+    return (
+      this.searchCat == "default" ||
+      this.searchLoc == "default" || 
+      this.searchText == "default")
+  }
   componentWillReceiveProps = nextProps => {
     this.getCredits(nextProps)
+    this.setState({searchText: nextProps.searchText})
+  }
+
+  openCatDialog = () => this.setState({ catVisible: true })
+
+  closeCatDialog = () => this.setState({ catVisible: false })
+
+  catChange = (value, index, event) => {
+    this.setState({ searchCat: value })
+    this.closeCatDialog()
+  }
+
+  getCatDialog = () => {
+    return(
+      <Dialog
+          id="publicationDialog"
+          visible={this.state.catVisible}
+          title="Seleccione Categoria"
+          onHide={this.closeCatDialog}
+        >
+        <SelectField
+            id="category"
+            label="Seleccione una categoria"
+            itemLabel="name"
+            itemValue="value"
+            value={this.state.searchCat}
+            menuItems={this.state.categories}
+            onChange={this.catChange}
+          />          
+        </Dialog>
+    )  
+  }
+
+  categoryChip = () => {
+    return  this.state.searchCat == "default"? "":
+     <Chip label={this.state.categories.find((x)=> {return x.value == this.state.searchCat}).name}
+      onClick={()=> {this.setState({searchCat: "default"})}}
+      removable
+      />
+  }
+
+  openLocDialog = () => this.setState({ locVisible: true })
+
+  closeLocDialog = () => this.setState({ locVisible: false })
+
+  locChange = (value, index, event) => {
+    this.setState({ searchLoc: value })
+    this.closeLocDialog()
+  }
+
+  getLocDialog = () => {
+    return(
+      <Dialog
+          id="locationDialog"
+          visible={this.state.locVisible}
+          title="Seleccione Ubicacion"
+          onHide={this.closeLocDialog}
+          className=""
+        >
+          <SelectField
+            id="state"
+            label="Seleccione un Lugar"
+            itemLabel="name"
+            itemValue="value"
+            value={this.state.state}
+            menuItems={this.state.states}
+            onChange={this.locChange}
+            className="md-cell stretch"
+          />
+        </Dialog>
+    )  
+  }
+
+  locationChip = () => {
+    return  this.state.searchLoc == "default"? "":
+     <Chip label={this.state.states.find((x)=> {return x.value == this.state.searchLoc}).name}
+      onClick={()=> {this.setState({searchLoc: "default"})}}
+      removable
+      />
   }
 
   openDialog = () => this.setState({ visible: true })
@@ -83,6 +204,45 @@ class PublicationList extends React.Component {
     return uid
   }
 
+  searchFilter = (x) =>{
+    return( 
+      ( x.title.toLowerCase().includes(this.state.searchText.toLowerCase() ) ) && 
+      ( this.state.searchLoc == "default" || x.state == this.state.searchLoc ) && 
+      ( this.state.searchCat == "default" || x.category == this.state.searchCat )
+    )
+  }
+
+  searchSort = (a, b) => {
+    if (this.isSearching()){
+      return b.user.score - a.user.score
+    }
+    else{
+      return b.submissions - a.submissions
+    }
+  }
+
+  searchHeader = () =>{
+  return (
+    <div>
+      <h2> {"Resultados para: " + this.state.searchText+"\t"} 
+      <Button
+        flat
+        disabled={this.state.searchLoc != "default"} 
+        label="Seleccionar Ubicacion" 
+        onClick={this.openLocDialog}
+      />
+      <Button
+        flat
+        disabled={this.state.searchCat != "default"}
+        label="Seleccionar Categoria"
+        onClick={this.openCatDialog}
+       />
+      </h2>
+      <Divider />
+    </div>
+      )
+  }
+
   getCredits = props => {
     rootRef
       .child("users/" + this.getUserId(props) + "/credits")
@@ -100,7 +260,7 @@ class PublicationList extends React.Component {
           <Button
             className="add-publication"
             floating
-            secundary
+            secondary
             onClick={this.toastFailure}
           >
             money_off
@@ -134,11 +294,19 @@ class PublicationList extends React.Component {
   render = () => {
     let publishButton = this.getButton()
     return (
+      <div>
+      {this.state.searchText !== ""? this.searchHeader() : ""}
+      {this.getLocDialog()}
+      {this.getCatDialog()}      
+      <div className="chip-list">
+        {this.locationChip()}
+        {this.categoryChip()}
+      </div>
       <publications>
         {this.state.publications
-          .sort(function(a, b) {
-            return b.submissions - a.submissions
-          })
+          //.filter(function(x) { return x.endDate > new Date(); })
+          .filter(this.searchFilter)
+          .sort(this.searchSort)
           .map(
             publication =>
               publication &&
@@ -147,6 +315,7 @@ class PublicationList extends React.Component {
         {publishButton}
         <Snackbar {...this.state} onDismiss={this._removeToast} />
       </publications>
+      </div>
     )
   }
 }
