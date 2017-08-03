a0ce7ae9b9d4a76de0fbd1bca7f216dceed4e273
Matias Pierobon <matias.pierobonm@gmail.com>
2017-08-03 03:29:26 -0300

    normalize styling to standar

--- src/components/ProfileView/ProfileMenu.jsx
+++ src/components/ProfileView/ProfileMenu.jsx
@@ -1,8 +1,8 @@
-import React, { PureComponent } from 'react';
-import Tabs from 'react-md/lib/Tabs/Tabs';
-import Tab from 'react-md/lib/Tabs/Tab';
-import TabsContainer from 'react-md/lib/Tabs/TabsContainer';
-import Slider from 'react-md/lib/Sliders';
+import React, { PureComponent } from "react"
+import Tabs from "react-md/lib/Tabs/Tabs"
+import Tab from "react-md/lib/Tabs/Tab"
+import TabsContainer from "react-md/lib/Tabs/TabsContainer"
+import Slider from "react-md/lib/Sliders"
 import FilteredPublicationList from "./FilteredPublicationList"
 import ProfileInformation from "./ProfileInformation"
 import ProfileCategories from "./ProfileCategories.jsx"
@@ -11,26 +11,26 @@ import _ from "lodash"
 import { userSelector } from "../../redux/getters"
 import { connect } from "react-redux"
 
-import CircularProgress from 'react-md/lib/Progress/CircularProgress';
+import CircularProgress from "react-md/lib/Progress/CircularProgress"
 
 @connect(state => ({ currentUser: userSelector(state) }))
 export default class ProfileMenu extends PureComponent {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = { 
-        activeTabIndex: 0, 
-        tabTwoChildren: null,
-        reports: [],
-        submissions: null,
-        currentUser: {admin: false},
-    };
-    this._handleTabChange = this._handleTabChange.bind(this);
+    this.state = {
+      activeTabIndex: 0,
+      tabTwoChildren: null,
+      reports: [],
+      submissions: null,
+      currentUser: { admin: false }
+    }
+    this._handleTabChange = this._handleTabChange.bind(this)
   }
 
   componentWillUnmount() {
     if (this._timeout) {
-      clearTimeout(this._timeut);
+      clearTimeout(this._timeut)
     }
   }
 
@@ -42,10 +42,10 @@ export default class ProfileMenu extends PureComponent {
 
   componentWillReceiveProps = nextProps => {
     this.getCurrentUser(nextProps.currentUser.uid)
-    this.getSubmissions()      
-    this.setState({activeTabIndex:0})
+    this.getSubmissions()
+    this.setState({ activeTabIndex: 0 })
   }
-  
+
   getCurrentUser = user => {
     rootRef
       .child("users")
@@ -53,23 +53,26 @@ export default class ProfileMenu extends PureComponent {
       .on("value", snap => this.setState({ currentUser: snap.val() }))
   }
 
-
-
   _handleTabChange(activeTabIndex) {
     if (activeTabIndex === 1 && !this.state.tabTwoChildren) {
       // Fake async loading
       this._timeout = setTimeout(() => {
-        this._timeout = null;
+        this._timeout = null
 
         this.setState({
           tabTwoChildren: [
-            <Slider id="slider" defaultValue={30} key="slider" className="md-cell md-cell--12" />,
-          ],
-        });
-      }, 3000);
+            <Slider
+              id="slider"
+              defaultValue={30}
+              key="slider"
+              className="md-cell md-cell--12"
+            />
+          ]
+        })
+      }, 3000)
     }
 
-    this.setState({ activeTabIndex });
+    this.setState({ activeTabIndex })
   }
 
   getSubmissions = () => {
@@ -81,11 +84,13 @@ export default class ProfileMenu extends PureComponent {
   }
 
   filterPublications = user => publication => {
-    return (publication.user === user && !publication.blocked && !publication.canceled)
+    return (
+      publication.user === user && !publication.blocked && !publication.canceled
+    )
   }
 
   filterReportedPublications = reports => publication => {
-    return reports.some(rep => rep.id == publication.id) 
+    return reports.some(rep => rep.id == publication.id)
   }
 
   getReports = () =>
@@ -105,50 +110,67 @@ export default class ProfileMenu extends PureComponent {
     })
 
   filterSubmissions = user => publication => {
-    if ((typeof this.state.submissions[publication.id] === 'undefined') || (publication.end < new Date()) || publication.canceled || publication.blocked) {
+    if (
+      typeof this.state.submissions[publication.id] === "undefined" ||
+      publication.end < new Date() ||
+      publication.canceled ||
+      publication.blocked
+    ) {
       return false
-    }else{
-      return typeof this.state.submissions[publication.id][user] !== 'undefined'
+    } else {
+      return typeof this.state.submissions[publication.id][user] !== "undefined"
     }
   }
 
   render() {
-    const { activeTabIndex } = this.state;
-    let { tabTwoChildren } = this.state;
+    const { activeTabIndex } = this.state
+    let { tabTwoChildren } = this.state
 
     if (!tabTwoChildren && activeTabIndex === 1) {
-      tabTwoChildren = <CircularProgress id="loading-tab-two" key="loading" />;
+      tabTwoChildren = <CircularProgress id="loading-tab-two" key="loading" />
     }
 
     return (
-      <TabsContainer onTabChange={this._handleTabChange} activeTabIndex={activeTabIndex} panelClassName="md-grid" colored>
+      <TabsContainer
+        onTabChange={this._handleTabChange}
+        activeTabIndex={activeTabIndex}
+        panelClassName="md-grid"
+        colored
+      >
         <Tabs tabId="tab">
-          <Tab label="Informacion" >
-            <ProfileInformation user={this.props.user}/>
+          <Tab label="Informacion">
+            <ProfileInformation user={this.props.user} />
           </Tab>
-          {(this.props.user === this.props.currentUser.uid || this.state.currentUser.admin) &&
-          <Tab label="Publicaciones">
-            <FilteredPublicationList filter={this.filterPublications(this.props.user)}/>
-          </Tab>
-          }
-          {(this.props.user === this.props.currentUser.uid || this.state.currentUser.admin) &&
-          <Tab label="Postulaciones">
-            {this.state.submissions && <FilteredPublicationList filter={this.filterSubmissions(this.props.user)}/>}
-          </Tab>
-          }
-          {(this.props.user === this.props.currentUser.uid && this.state.currentUser.admin) &&
-          <Tab label="Publicaciones Reportadas">
-            <FilteredPublicationList filter={this.filterReportedPublications(this.state.reports)}/> 
-          </Tab>
-          }
-          {(this.props.user === this.props.currentUser.uid && this.state.currentUser.admin) &&
-          <Tab label="Categorias">
-            <ProfileCategories test="asd"/> 
-          </Tab>
-          }
-
+          {(this.props.user === this.props.currentUser.uid ||
+            this.state.currentUser.admin) &&
+            <Tab label="Publicaciones">
+              <FilteredPublicationList
+                filter={this.filterPublications(this.props.user)}
+              />
+            </Tab>}
+          {(this.props.user === this.props.currentUser.uid ||
+            this.state.currentUser.admin) &&
+            <Tab label="Postulaciones">
+              {this.state.submissions &&
+                <FilteredPublicationList
+                  filter={this.filterSubmissions(this.props.user)}
+                />}
+            </Tab>}
+          {this.props.user === this.props.currentUser.uid &&
+            this.state.currentUser.admin &&
+            <Tab label="Publicaciones Reportadas">
+              <FilteredPublicationList
+                canReport={true}
+                filter={this.filterReportedPublications(this.state.reports)}
+              />
+            </Tab>}
+          {this.props.user === this.props.currentUser.uid &&
+            this.state.currentUser.admin &&
+            <Tab label="Categorias">
+              <ProfileCategories test="asd" />
+            </Tab>}
         </Tabs>
       </TabsContainer>
-    );
+    )
   }
 }
